b1c75ba529226a3c3444943cf7b6be66cf7297b2
Andrew Romanenco <andrew@romanenco.com>
2016-12-18 15:41:15 -0500

    Add pattern matching for lexer

--- Lexer.hs
+++ Lexer.hs
@@ -6,9 +6,28 @@ module Lexer where
 
 import Data.List
 import Data.Maybe
+import Text.Regex.PCRE
 
 
 -- | Cuts off existing prefix from a string. Attempt to cut off non existing
 -- prefix will fail at runtime.
 stripExistingPrefix :: [Char] -> [Char] -> [Char]
 stripExistingPrefix  prefix str = fromJust (stripPrefix prefix str)
+
+
+-- | Case insensitive regex: http://stackoverflow.com/a/1008334
+(=~+) ::
+   ( RegexMaker regex compOpt execOpt source
+   , RegexContext regex source1 target )
+   => source1 -> (source, compOpt, execOpt) -> target
+source1 =~+ (source, compOpt, execOpt)
+  = match (makeRegexOpts compOpt execOpt source) source1
+
+
+-- | Match POSIX regex to the beginning of given list. Case insensitive.
+-- Result is token matched and the rest of the list; or nothing.
+match_token :: [Char] -> [Char] -> Maybe([Char], [Char])
+match_token pattern str = let matched = (str =~+ ("^" ++ pattern, compCaseless + compUTF8, execBlank) :: String)
+ in if matched == []
+   then Nothing
+   else Just (matched, stripExistingPrefix matched str)
--- LexerSpec.hs
+++ LexerSpec.hs
@@ -8,3 +8,13 @@ main = hspec $ do
   describe "stripExistingPrefix" $ do
     it "curs off existing prefix" $
       stripExistingPrefix "pre" "prefix" `shouldBe` "fix"
+
+  describe "match_token" $ do
+    it "matches pattern to the front" $
+      match_token "[0-9]+" "123abc" `shouldBe` Just ("123", "abc")
+
+    it "does not matches if not in the front" $
+      match_token "999" "x999abc" `shouldBe` Nothing
+
+    it "match is case insensitive" $
+      match_token "abc" "aBc123" `shouldBe` Just ("aBc", "123")
