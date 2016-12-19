3e3855230166f9fef595376f51ca10953a1f5cc6
Andrew Romanenco <andrew@romanenco.com>
2016-12-19 02:04:30 -0500

    Add lexer tests for example pascal code

--- LexerSpec.hs
+++ LexerSpec.hs
@@ -285,3 +285,60 @@ main = hspec $ do
 
     it "expression" $
       tokenize "if x<y then a:=45" `shouldBe` [IF, WS " ", IDENT "x", LT_,IDENT "y",WS " ", THEN, WS " ", IDENT "a", ASSIGN, NUM_INT "45", EOF]
+
+  describe "tokenize samples" $ do
+    it "subscripts.pas" $ do
+      input <- readFile "res/examples/subscripts.pas"
+      tokenize input `shouldEndWith` [EOF]
+
+    it "set.pas" $ do
+      input <- readFile "res/examples/set.pas"
+      tokenize input `shouldEndWith` [EOF]
+
+    it "pointer.pas" $ do
+      input <- readFile "res/examples/pointer.pas"
+      tokenize input `shouldEndWith` [EOF]
+
+    it "passfail.pas" $ do
+      input <- readFile "res/examples/passfail.pas"
+      tokenize input `shouldEndWith` [EOF]
+
+    it "nesting.pas" $ do
+      input <- readFile "res/examples/nesting.pas"
+      tokenize input `shouldEndWith` [EOF]
+
+    it "linkedlist2.pas" $ do
+      input <- readFile "res/examples/linkedlist2.pas"
+      tokenize input `shouldEndWith` [EOF]
+
+    it "if.pas" $ do
+      input <- readFile "res/examples/if.pas"
+      tokenize input `shouldEndWith` [EOF]
+
+    it "helloworld.pas" $ do
+      input <- readFile "res/examples/helloworld.pas"
+      tokenize input `shouldEndWith` [EOF]
+
+    it "fact.pas" $ do
+      input <- readFile "res/examples/fact.pas"
+      tokenize input `shouldEndWith` [EOF]
+
+    it "case.pas" $ do
+      input <- readFile "res/examples/case.pas"
+      tokenize input `shouldEndWith` [EOF]
+
+    it "bubble.pas" $ do
+      input <- readFile "res/examples/bubble.pas"
+      tokenize input `shouldEndWith` [EOF]
+
+    it "array2.pas" $ do
+      input <- readFile "res/examples/array2.pas"
+      tokenize input `shouldEndWith` [EOF]
+
+    it "array.pas" $ do
+      input <- readFile "res/examples/array.pas"
+      tokenize input `shouldEndWith` [EOF]
+
+    it "add.pas" $ do
+      input <- readFile "res/examples/add.pas"
+      tokenize input `shouldEndWith` [EOF]
