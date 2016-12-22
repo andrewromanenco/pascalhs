913880f62e3e1436154dc6c715d8f81413ea3148
Andrew Romanenco <andrew@romanenco.com>
2016-12-22 03:15:38 -0500

    Implemented program heading read

--- parser.hs
+++ parser.hs
@@ -6,25 +6,55 @@ import LexerTokens
 
 
 -- Program's structures for AST.
-data ProgramHeading = ProgramHeading String
+data ProgramHeading = ProgramHeading String (Maybe IdentifierList)
                     | UnitHeading String
                     deriving(Show, Eq)
 
+data IdentifierList = IdentifierList [String]
+                    deriving (Show, Eq)
+
+idList :: IdentifierList -> [String]
+idList (IdentifierList values) = values
+
+idListPrepend :: String -> IdentifierList -> IdentifierList
+idListPrepend name lst = IdentifierList ([name] ++ idList lst)
 
 -- Parsing elements.
 
-parseProgramHeading :: [Token] -> Maybe (ProgramHeading, [Token])
-parseProgramHeading (UNIT _: restOfInput) = let mName = parseIdentifier restOfInput
-  in case mName of
-    Nothing -> error "Can't find name identifier for UNIT"
-    Just (name, restOfInput) -> Just (UnitHeading name, semiExpected restOfInput)
-parseProgramHeading _ = Nothing
+parseProgramHeading :: [Token] -> (ProgramHeading, [Token])
+parseProgramHeading (UNIT _: restOfInput) = let (name, rest) = parseIdentifier restOfInput
+  in (UnitHeading name, semiExpected rest)
+parseProgramHeading (PROGRAM _: restOfInput) = let (name, rest) = parseIdentifier restOfInput
+  in case head rest of
+    LPAREN _ -> let (values, deepRest) = parseIdentifierList(tail rest)
+              in ((ProgramHeading name (Just values), semiExpected (rparenExpected deepRest)))
+    otherwise -> (ProgramHeading name Nothing, semiExpected rest)
+parseProgramHeading (x:_) = tokenExpectationError "Program or unit declaration" x
 
-parseIdentifier :: [Token] -> Maybe (String, [Token])
-parseIdentifier (IDENT _ name:restOfInput) = Just (name, restOfInput)
-parseIdentifier _ = Nothing
+
+parseIdentifierList :: [Token] -> (IdentifierList, [Token])
+parseIdentifierList input@(IDENT _ _:_) = let (name, restOfInput) = parseIdentifier input
+  in case head restOfInput of
+    COMMA _ -> let (list, rest) = parseIdentifierList(tail restOfInput)
+               in (idListPrepend name list, rest)
+    otherwise -> (IdentifierList [name], restOfInput)
+parseIdentifierList (x:_) = tokenExpectationError "Identifier" x
+
+
+parseIdentifier :: [Token] -> (String, [Token])
+parseIdentifier (IDENT _ name:restOfInput) = (name, restOfInput)
+parseIdentifier (x:_) = tokenExpectationError "Identifier" x
 
 semiExpected :: [Token] -> [Token]
 semiExpected (SEMI _:restOfInput) = restOfInput
-semiExpected (EOF:_) = error "Semicolon expected, but end of file reached."
-semiExpected (x:_) = error ("Semicolon expected, but not found at " ++ show(cursorValue(x)))
+semiExpected (x:_) = tokenExpectationError "Semicolon" x
+
+rparenExpected :: [Token] -> [Token]
+rparenExpected (RPAREN _:restOfInput) = restOfInput
+rparenExpected (x:_) = tokenExpectationError "RPAREN" x
+
+
+tokenExpectationError :: String -> Token -> a
+tokenExpectationError tokenNameExpected token = case token of
+  EOF -> error (tokenNameExpected ++ " expected but end of file reached")
+  otherwise -> error (tokenNameExpected ++" expected at "++ show(cursorValue(token)))
--- tests/ParserSpec.hs
+++ tests/ParserSpec.hs
@@ -8,11 +8,20 @@ main :: IO ()
 c = Cursor 3 4
 main = hspec $ do
   describe "parse elements" $ do
-    it "program heading: unit" $
-      parseProgramHeading [UNIT c, IDENT c "name", SEMI c] `shouldBe` Just (UnitHeading "name", [])
-
     it "identifier" $ do
-      parseIdentifier [IDENT c "name", EOF] `shouldBe` Just("name", [EOF])
+      parseIdentifier [IDENT c "name", EOF] `shouldBe` ("name", [EOF])
 
-    it "identifier not found" $ do
-      parseIdentifier [EOF] `shouldBe` Nothing
+    it "IdentifierList single" $ do
+      parseIdentifierList [IDENT c "name", EOF] `shouldBe` (IdentifierList ["name"], [EOF])
+
+    it "IdentifierList multiple" $ do
+      parseIdentifierList [IDENT c "name1", COMMA c, IDENT c "name2", COMMA c, IDENT c "name3", EOF]
+        `shouldBe` (IdentifierList ["name1", "name2", "name3"], [EOF])
+
+    it "program heading: program" $
+      parseProgramHeading [PROGRAM c, IDENT c "name", SEMI c]
+        `shouldBe` (ProgramHeading "name" Nothing, [])
+
+    it "program heading: program with id list" $
+      parseProgramHeading [PROGRAM c, IDENT c "name", LPAREN c, IDENT c "var", RPAREN c, SEMI c]
+        `shouldBe` (ProgramHeading "name" (Just( IdentifierList ["var"])), [])
