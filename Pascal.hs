01ad2f9ec2f6a52eea116d6ba267210c6bb0bb07
Andrew Romanenco <andrew@romanenco.com>
2016-12-24 14:44:05 -0500

    Add console interface

---
+++ Pascal.hs
@@ -0,0 +1,33 @@
+-- | Console interface to pascal interpreter.
+-- Prints tokens, abstract syntax tree or interprets a program.
+
+module Main where
+
+import System.Environment
+
+import Lexer
+import Parser
+
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    ("lex":path:_) -> runLexer path
+    ("ast":path:_) -> runParser path
+    ("run":path:_) -> runInterpreter path
+    (path:_) -> runInterpreter path
+    _ -> putStrLn "Params: [lex|ast|run] path_to_pascal"
+
+runLexer :: String -> IO ()
+runLexer path = do
+  sourceCode <- readFile path
+  putStrLn (show (tokenize sourceCode))
+
+runParser :: String -> IO ()
+runParser path = do
+  sourceCode <- readFile path
+  putStrLn (show (parse (tokenize sourceCode)))
+
+runInterpreter :: String -> IO ()
+runInterpreter path = putStrLn "Interpreter is work-in-progress"
